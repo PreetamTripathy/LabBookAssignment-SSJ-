347d28b46c810bfe77da58bb338e3aec044888b8
PreetamTripathy <61135000+PreetamTripathy@users.noreply.github.com>
2020-04-16 09:55:47 +0530

    Add files via upload

---
+++ servlet/ServletsLab3/src/com/cg/iter/service/TrainingService.java
@@ -0,0 +1,12 @@
+  
+package com.cg.iter.service;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import com.cg.iter.bean.Training;
+
+public interface TrainingService {
+	public List<Training> getAllTrainingDetails() throws SQLException;
+	public String UpdateAvailableseats(int id) throws SQLException;
+}
